4e7bd8b64cc841be4c667d384618a1b1bba93363
Edgar1us <edgarbc106@gmail.com>
2021-08-27 19:16:57 +0200

    registro y cambios en diseño

---
+++ app/src/main/java/edbeca/simarro/marketv1/activities/CrearCuentaActivity.java
@@ -0,0 +1,16 @@
+package edbeca.simarro.marketv1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edbeca.simarro.marketv1.R;
+
+public class CrearCuentaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_cuenta);
+    }
+}
